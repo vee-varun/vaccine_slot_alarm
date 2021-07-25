be11cc0057631a4fe2bf5df2af11eeadaf1e7e9e
Varun Taneja <tanejavarun1995@gmail.com>
2021-07-25 18:09:21 +0530

    added district options

--- vaccine_slot_alarm.py
+++ vaccine_slot_alarm.py
@@ -1,7 +1,10 @@
+import re
 import requests
 import datetime
 from pydub import AudioSegment
 from pydub.playback import play
+
+from constants import DISTRICT_IDS
 
 API_URL = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict'
 DAY_RANGE = 1  # for how many days you want to check the slot
@@ -18,20 +21,56 @@ HEADERS = {
     'Accept-Language': 'hi_IN',
 }
 
-districts = {
-    'South West Delhi': 150,
-    'West Delhi': 142,
-    'Central Delhi': 141,
-    'South Delhi': 149,
-    'South East Delhi': 144,
-    'New Delhi': 140,
-    'East Delhi': 145,
-    'North Delhi': 146,
-    'North East Delhi': 147,
-    'North West Delhi': 143,
-    'Shahdara': 148
+for d in DISTRICT_IDS:
+    print(f'Pass {d["state_id"]} state id for {d["state_name"]}.')
+print('\n')
+while True:
+    try:
+        state_id = input('In which state you want to search for slots? : ')
+        state_id = int(state_id)
+        districts = None
+        for d in DISTRICT_IDS:
+            if d['state_id'] == state_id:
+                districts = d['districts']
+                state_name = d['state_name']
+        if not districts:
+            print(f'Invalid state_id passed.', end='\n\n')
+        else:
+            break
+    except Exception:
+        print(f'Invalid state_id passed.', end='\n\n')
 
-}
+print('\n')
+
+f_districts = {}
+for d in districts:
+    print(f'Input {d["district_id"]} for {d["district_name"]}.')
+while True:
+    print('\n')
+    district_ids = input(
+        f'In which district of {state_name}? You can pass multiple district '
+        f'ids separated with comma : '
+    )
+    m = re.match('([0-9]+,\s*)*[0-9]+,?$', district_ids)
+    if not m:
+        print(f'1-Invalid input - {district_ids}. Please check.')
+        continue
+    district_ids = district_ids.split(',')
+    try:
+        district_ids = [int(id) for id in district_ids]
+        for id in district_ids:
+            for district in districts:
+                if district['district_id'] == id:
+                    f_districts[district['district_name']] = district['district_id']
+                    break
+            else:
+                print(f'Invalid district id found - {id}')
+                break
+        if f_districts:
+            break
+    except ValueError:
+        print(f'2-Invalid input - {district_ids}. Please check.')
+
 
 c = 0
 while True:
@@ -40,13 +79,20 @@ while True:
     for i in range(DAY_RANGE):
         date = (datetime.datetime.now() + datetime.timedelta(days=i + 1)).strftime('%d-%m-%Y')
         print(f'*******************************************{date} *******************************************')
-        for district_name, district_id in districts.items():
+        for district_name, district_id in f_districts.items():
             print(f'Checking slots in {district_name}.')
             params = (
                 ('district_id', district_id),
                 ('date', date),
             )
-            response = requests.get(API_URL, headers=HEADERS, params=params)
+            try:
+                response = requests.get(API_URL, headers=HEADERS, params=params)
+            except Exception:
+                print(
+                    'Failed to request the API URL. Please check your internet connection.'
+                )
+            if not response.ok:
+                print('Not working. Please try after some time.')
             data = response.json()
             centers = data['sessions']
             for center in centers:
